340e909a7fcfbde19491066cf76418425b6cd874
Just-a-Unity-Dev <rnmangunay@addu.edu.ph>
2022-04-16 23:49:21 +0800

    migrate to a library

--- index.js
+++ index.js
@@ -1,35 +1,4 @@
-const { app, BrowserWindow, shell } = require("electron");
-
 const dev = !app.isPackaged;
 const config = require("./spirus.config");
 
-const discordClientID = config.discordRPC.clientID;
-let discordClient = null;
-if (config.discordRPC.enabled == true)
-    discordClient = require("discord-rich-presence")(discordClientID);
-
-function createWindow() {
-    const win = new BrowserWindow({
-        width: config.window.width,
-        height: config.window.height,
-        icon: "./assets/favicon.png",
-    });
-
-    win.webContents.addListener("new-window", (e, url) => {
-        e.preventDefault();
-        shell.openExternal(url);
-    });
-
-    if (dev) win.loadURL(`http://localhost:${process.env.ELECTRON_WEBPACK_PORT}`);
-    else win.loadFile("./dist/index.html");
-}
-
-app.on("ready", () => createWindow);
-
-app.on("activate", () => {
-    if (BrowserWindow.getAllWindows().length === 0) createWindow();
-});
-
-app.on("window-all-closed", () => {
-    if (process.platform !== "darwin") app.quit();
-});
+console.log(config)
--- spirus.config.js
+++ spirus.config.js
@@ -4,19 +4,5 @@ module.exports = {
 	window: {
 		width: 1920,
 		height: 1080
-	},
-	discordRPC: {
-		enabled: false, // feel free to disable
-		clientID: "", // set to your client ID
-
-		config: {
-			state: "Spirus!", // state of the game
-			details: "https://github.com/EclipseLikesSpace/Spirus", // the details
-			startTimestamp: Date.now(),
-			endTimestamp: null, // leave null for "Time elapsed"
-			largeImageKey: "spirus_large", // need to set these in your Discord Developer Portal
-			smallImageKey: "spirus_small", // ^
-			instance: true, // leave this true
-		}
-	},
+	}
 }
