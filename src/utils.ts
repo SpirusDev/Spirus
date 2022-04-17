e30cb55f45be6ea55264ef67ef2777eb041cf597
blaumeise20 <62756994+blaumeise20@users.noreply.github.com>
2022-04-17 12:00:43 +0200

    Update a lot of stuff

--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,5 @@
-import GameObject from "./classes/object";
+import GameObject from "./GameObject";
 import { Collision } from "./types";
-import { objects } from './classes/object';
 
 export function isColliding(object1: GameObject, object2: GameObject): Collision {
     let aLeftOfB = (object1.position.x + object1.scale.x) < object2.position.x;
@@ -22,6 +21,18 @@ export function isVectorOOB(vector: number) {
     return vector <= -70 || vector >= 70;
 }
 
-export function findObjectOfClass(classType: Function) {
-    return objects.find(c => c instanceof classType)
+export function run(fn: (delta: number) => void): void {
+    let last = performance.now();
+    let delta = 0;
+
+    function loop(now: number) {
+        delta = now - last;
+        last = now;
+
+        fn(delta);
+
+        requestAnimationFrame(loop);
+    }
+
+    loop(performance.now());
 }
